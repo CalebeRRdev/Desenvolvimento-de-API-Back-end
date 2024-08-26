120e05e6a4d0f47e162449e9435097ebda3a7f80
Calebe Rodrigues Rolim <125617377+CalebeRRdev@users.noreply.github.com>
2024-08-26 08:36:58 -0300

    Update api.php

--- sistema_pizzaria-master/routes/api.php
+++ sistema_pizzaria-master/routes/api.php
@@ -7,4 +7,6 @@ Route::post('/cadastrar', [UserController::class, 'store']);
 
 Route::prefix('/user')->group(function (){
     Route::get('/', [UserController::class, 'index']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
 });
