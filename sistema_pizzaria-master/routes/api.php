f5feaecd1123586e38b603fbc933f6d1ad1345e8
Calebe Rodrigues Rolim <125617377+CalebeRRdev@users.noreply.github.com>
2024-08-27 10:46:30 -0300

    Update api.php

--- sistema_pizzaria-master/routes/api.php
+++ sistema_pizzaria-master/routes/api.php
@@ -5,14 +5,18 @@ use Illuminate\Support\Facades\Route;
 
 /**
     * Modificação realizada por Calebe Rodrigues Rolim
-    * Data: 26/08/2024
-    * Hora: 13:10
+    * Data: 27/08/2024
+    * Hora: 10:30
     *
     * Descrição:
-    * - Adicionada rota PUT para atualizar um usuário: /user/{id}.
-     * - Adicionada rota DELETE para deletar um usuário: /user/{id}.
-    * - Rotas organizadas sob o prefixo '/user'.
-    */ 
+    * - Mantida a rota POST para cadastrar um usuário: /cadastrar.
+    * - Reorganizadas as rotas sob o prefixo '/user':
+    *   - GET /user: Lista todos os usuários.
+    *   - PUT /user/{id}: Atualiza um usuário existente.
+    *   - DELETE /user/{id}: Deleta um usuário existente.
+    *   - POST /user/cadastrar: Cadastra um novo usuário.
+    * - Removida a rota POST /cadastrar do prefixo '/user' para evitar duplicação.
+    */
 
 Route::post('/cadastrar', [UserController::class, 'store']);
 
@@ -20,4 +24,6 @@ Route::prefix('/user')->group(function (){
     Route::get('/', [UserController::class, 'index']);
     Route::put('/{id}', [UserController::class, 'update']);
     Route::delete('/{id}', [UserController::class, 'destroy']);
+    Route::post('/cadastrar', [UserController::class, 'store']);
 });
+
